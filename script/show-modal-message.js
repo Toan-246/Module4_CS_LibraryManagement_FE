ba77562897a243991bb3e742cc044847ee9d6312
LuytBui <luytbq43@gmail.com>
2022-04-14 17:14:10 +0700

    add modal

---
+++ script/show-modal-message.js
@@ -0,0 +1,12 @@
+
+function showModalMessage(){
+    $("message-modal").toggleClass("show", true);
+}
+function hideModalMessage(){
+    $("message-modal").toggleClass("show", false);
+}
+
+$(document).ready(function () {
+    showModalMessage();
+    $("#pre")
+});
