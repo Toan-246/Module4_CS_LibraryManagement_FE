4da01a6a504249541e7158ea34595f42edbed363
LuytBui <luytbq43@gmail.com>
2022-04-16 09:33:05 +0700

    /

--- script/init.js
+++ script/init.js
@@ -1,6 +1,8 @@
+
 let USERS_WEBSERVICE_LOGIN = "http://localhost:8080/api/login";
 let USERS_WEBSERVICE_REGISTER = "http://localhost:8080/api/register";
 let USERS_WEBSERVICE_CHANGE_PASSWORD = "http://localhost:8080/api/changePassword";
+let USERS_WEBSERVICE_GET_INFO = "http://localhost:8080/api/users";
 
 let PRODUCTS_WEBSERVICE_ROOT = "http://localhost:8080/api/products";
 let CATEGORIES_WEBSERVICE_ROOT = "http://localhost:8080/api/categories";
